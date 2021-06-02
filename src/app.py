c1b4434911506728fabc18e136a027d9f6aae56d
w91379137 <w913791379137@gmail.com>
2021-06-02 16:44:02 +0800

    add index.html

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    return 'hello'
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
